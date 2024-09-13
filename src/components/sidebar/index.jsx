0b18bdd2cba9a092b51abffd7df56e93f3259ae4
Ryan <ryanjerickson@gmail.com>
2024-09-13 00:08:38 -0500

    sidebar hr

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -5,8 +5,15 @@ function Sidebar() {
 
     return <div className="sidebar-content">
         <h1>Ryan Does It</h1>
+        <hr></hr>
         <ul>
             <li>Home</li>
+            <li>Games</li>
+            <li>Software</li>
+            <li>Music</li>
+            <li>Video</li>
+            <li>Audio</li>
+            <li>Manufacturing</li>
         </ul>
     </div>
 }
