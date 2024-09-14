1edab1ad08eb4a0af8970a5babd51b78a31636e9
Ryan <ryanjerickson@gmail.com>
2024-09-14 16:30:19 -0500

    contact info

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -6,6 +6,9 @@ function Contact() {
 
     return <div className='contact-content'>
         <h2>Contact Me</h2>
+        <p>Ready to inquire about a service or hiring information? <br/> Send an email to:</p>
+            <a href='mailto:ryan@ryandoesit.com'>Ryan@RyanDoesIt.com</a>
+        <p>Thanks for visiting and I'll get back to you soon!</p>
         </div>
 }
 
