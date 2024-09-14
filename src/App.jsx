9075f7854b8a39c4403c85faf12b3b853361b8fd
Ryan <ryanjerickson@gmail.com>
2024-09-14 15:53:08 -0500

    skeleton routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './components/home'
 import Sidebar from './components/sidebar'
+import Home from './components/home'
 import Games from "./components/games";
+import Software from "./components/software";
+import Music from "./components/music";
+import Video from "./components/video";
+import Audio from "./components/audio";
+import Voiceover from "./components/voiceover";
+import Contact from "./components/contact";
 import './App.css'
+
 
 function App() {
 
@@ -14,6 +21,12 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="games" element={<Games />} />
+            <Route path="software" element={<Software />} />
+            <Route path="music" element={<Music />} />
+            <Route path="video" element={<Video />} />
+            <Route path="audio" element={<Audio />} />
+            <Route path="voiceover" element={<Voiceover />} />
+            <Route path="contact" element={<Contact />} />
           </Routes>
         </div>
       </div>
